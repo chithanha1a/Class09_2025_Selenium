4b5b904dc1b638c518f5ad2817380546e73e81a4
UT IT <huuphuc310511@gmail.com>
2025-10-22 19:55:48 +0700

    update 22/10

--- Automation/src/main/java/automation/common/CommonBase.java
+++ Automation/src/main/java/automation/common/CommonBase.java
@@ -1,5 +1,13 @@
 package automation.common;
 
+import org.openqa.selenium.WebDriver;
+
 public class CommonBase {
+	public static WebDriver driver;
+	public WebDriver initWebDriver(String URL)
+	
+	{
+		return ;
+	}
 	
 }
--- Automation/src/main/java/automation/constant/CT_PageURL.java
+++ Automation/src/main/java/automation/constant/CT_PageURL.java
@@ -1,5 +1,7 @@
 package automation.constant;
 
 public class CT_PageURL {
+	public String RISE_URL="https://rise.fairsketch.com/index.php/signin";
+	
 
 }
