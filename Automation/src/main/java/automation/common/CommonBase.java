fdb72c62ae1de3296cd6e357878d888d94e260ed
UT IT <huuphuc310511@gmail.com>
2025-10-22 21:29:50 +0700

    khong co gi

--- Automation/src/main/java/automation/common/CommonBase.java
+++ Automation/src/main/java/automation/common/CommonBase.java
@@ -1,21 +1,21 @@
 package automation.common;
 
 import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class CommonBase {
 	public static WebDriver driver;
-	public WebDriver initWebDriver(String URL)
-	
+	public WebDriver initWebDriver(String URL)	
 	{
 		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\driver\\chromedriver.exe");
-		FirefoxDriver driver = new FirefoxDriver();
-		//ChromeDriver driver = new ChromeDriver();
+		//FirefoxDriver driver = new FirefoxDriver();
+		ChromeDriver driver = new ChromeDriver();
 		driver.get(URL);
 		driver.manage().window().maximize();
 		return driver;
 	}
+	
 	public void closeDriver()
 	{
 		if(driver!=null)
--- Automation/src/main/java/automation/constant/CT_PageURL.java
+++ Automation/src/main/java/automation/constant/CT_PageURL.java
@@ -2,6 +2,6 @@ package automation.constant;
 
 public class CT_PageURL {
 	public static String RISE_URL="https://rise.fairsketch.com/index.php/signin";
-	
+	public static String BEPANTOAN_URL="https://bepantoan.vn/";
 
 }
