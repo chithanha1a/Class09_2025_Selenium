a753428c954477b61b353c57d0a1ce3472ad40a6
UT IT <huuphuc310511@gmail.com>
2025-10-23 20:27:28 +0700

    update bai tap 1-2

--- Automation/src/main/java/automation/constant/CT_PageURL.java
+++ Automation/src/main/java/automation/constant/CT_PageURL.java
@@ -3,5 +3,6 @@ package automation.constant;
 public class CT_PageURL {
 	public static String RISE_URL="https://rise.fairsketch.com/index.php/signin";
 	public static String BEPANTOAN_URL="https://bepantoan.vn/";
-
+	public static String Selector_URL="https://selectorshub.com/xpath-practice-page/";
+	public static String GITHUB_URL="https://automationfc.github.io/basic-form/index.html";
 }
