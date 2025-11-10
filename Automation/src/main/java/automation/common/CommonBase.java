a60bc82c69cafcb453981dead1d21e8e3c9faca4
UT IT <huuphuc310511@gmail.com>
2025-11-10 20:49:08 +0700

    10-11-2025

--- Automation/src/main/java/automation/common/CommonBase.java
+++ Automation/src/main/java/automation/common/CommonBase.java
@@ -1,18 +1,22 @@
 package automation.common;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 //import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class CommonBase {
 	public static WebDriver driver;
-	public WebDriver initWebDriver(String URL)	
+	public static WebDriver initWebDriver(String URL)	
 	{
 		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\driver\\chromedriver.exe");
 		//FirefoxDriver driver = new FirefoxDriver();
 		ChromeDriver driver = new ChromeDriver();
 		driver.get(URL);
 		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout( Duration.ofSeconds(30));
 		return driver;
 	}
 	
@@ -23,5 +27,19 @@ public class CommonBase {
 			driver.close();
 		}
 	}
+	public static WebDriver initFirefoxDriver(String URL) {
+		System.setProperty("webdriver.firefox.driver", System.getProperty("user.dir") + "\\driver\\geckodriver.exe");
+		FirefoxDriver driver = new FirefoxDriver();
+		driver.get(URL);
+		driver.manage().window().maximize();
+		return driver;
+	}
 	
+	public WebDriver initChromeDriver(String URL) {
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\driver\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get(URL);
+		driver.manage().window().maximize();
+		return driver;
+	}
 }
---
+++ Automation/src/main/java/automation/testsuite/Day12_Dropdownlist.java
@@ -0,0 +1,33 @@
+package automation.testsuite;
+
+import automation.common.CommonBase;
+import automation.constant.CT_PageURL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+public class Day12_Dropdownlist extends CommonBase{
+	
+	@Test
+	public void selectDropdownlist()
+	{
+		driver = initChromeDriver(CT_PageURL.CODESTART_URL);
+Select dropCourse = new Select(driver.findElement(By.id("product_categories_filter")));
+        
+        // Kiểm tra size
+        int size = dropCourse.getOptions().size();
+        System.out.println("Size is: " + size);
+
+        // Chọn AWS theo cách 1
+        dropCourse.selectByVisibleText("AWS");
+        String aws = dropCourse.getFirstSelectedOption().getText();
+        System.out.println("Text sau khi chọn AWS: " + aws);
+
+        // Chọn Lập trình web theo cách 2
+        dropCourse.selectByValue("https://codestar.vn/product-category/lap-trinh-web/");
+        String laptrinhWeb = dropCourse.getFirstSelectedOption().getText();
+        System.err.println("Text sau khi chọn Lập trình web: " + laptrinhWeb);
+	}
+
+}
