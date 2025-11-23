c46c303c99a9198d33bdebb2fb80a20ba1f2ffff
UT IT <huuphuc310511@gmail.com>
2025-11-23 10:03:04 +0700

    update 23/11/2025

--- Automation/src/main/java/automation/constant/CT_PageURL.java
+++ Automation/src/main/java/automation/constant/CT_PageURL.java
@@ -9,4 +9,5 @@ public class CT_PageURL {
 	public static String CODESTART_URL="https://codestar.vn/product-category/aws/";
 	public static String GLOBAL_URL="https://www.globalsqa.com/demo-site/select-dropdown-menu/";
 	public static String SELENIUM_URL="https://automationfc.github.io/basic-form/index.html";
+	public static String CRMSTAR_URL="https://test-system.crmstar.vn/";
 }
